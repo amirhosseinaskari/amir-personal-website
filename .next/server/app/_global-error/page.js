5591df48ee90516e2f1369e853e3b6a630378f02
amiraskari.it@gmail.com <amiraskari.it@gmail.com>
2026-05-24 13:52:16 +0100

    change

--- .next/server/app/_global-error/page.js
+++
@@ -1,9 +0,0 @@
-var R=require("../../chunks/ssr/[turbopack]_runtime.js")("server/app/_global-error/page.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0sb_5~_._.js")
-R.c("server/chunks/ssr/0kic_next_dist_esm_build_templates_app-page_0mewfqx.js")
-R.c("server/chunks/ssr/[root-of-the-server]__01kw88.._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0ap5-9s._.js")
-R.c("server/chunks/ssr/0kic_next_dist_client_components_builtin_global-error_0gw746f.js")
-R.c("server/chunks/ssr/_next-internal_server_app__global-error_page_actions_0k77kol.js")
-R.m(65997)
-module.exports=R.m(65997).exports
